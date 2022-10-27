7de27c2524c1126a748b5d95b1301ab65ec912f1
dauntless <suntopmultimedia@gmail.com>
2022-10-27 14:51:59 +0100

    requirements

--- hngApi/views.py
+++ hngApi/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 def index_view(request):
     return JsonResponse(
         {
-            'slackUsername' : 'Oluwaseun Matanmi', 
+            'slackUsername' : 'omatanmi', 
             'backend' : True, 'age' : 20, 'bio' : 'A focused, talented young boy, who is optimistic on becoming the next sensation. I love python, Javascript & Virtual Reality'
         }
     )
