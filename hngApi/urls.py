a8786750426c4467330f51d57119850b8e698c84
dauntless <suntopmultimedia@gmail.com>
2022-10-31 17:31:21 +0100

    api

--- hngApi/urls.py
+++ hngApi/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index_view
+from .views import IndexView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index_view),
+    path('', IndexView.as_view()),
     path('admin/', admin.site.urls),
 ]
 
--- hngApi/views.py
+++ hngApi/views.py
@@ -1,9 +1,11 @@
-from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-def index_view(request):
-    return JsonResponse(
-        {
+class IndexView(APIView):
+    def get(self, request, format=None):
+        data = {
             'slackUsername' : 'omatanmi', 
             'backend' : True, 'age' : 20, 'bio' : 'A focused, talented young boy, who is optimistic on becoming the next sensation. I love python, Javascript & Virtual Reality'
         }
-    )
+        return Response(data, status=status.HTTP_200_OK)
