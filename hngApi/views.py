75e5b654daa35dc7df52063439dc9905a4608598
dauntless <suntopmultimedia@gmail.com>
2022-10-31 17:37:41 +0100

    api

--- hngApi/views.py
+++ hngApi/views.py
@@ -8,4 +8,8 @@ class IndexView(APIView):
             'slackUsername' : 'omatanmi', 
             'backend' : True, 'age' : 20, 'bio' : 'A focused, talented young boy, who is optimistic on becoming the next sensation. I love python, Javascript & Virtual Reality'
         }
-        return Response(data, status=status.HTTP_200_OK)
+        headers = {
+            'accept' : '*',
+            'Content-Type' : 'application/json'
+        }
+        return Response(data, status=status.HTTP_200_OK, headers=headers)
