2e8719dc2700aa55ea4b9c70b49acb338be9e034
dauntless <suntopmultimedia@gmail.com>
2022-11-04 07:25:42 +0100

    regex

--- hngApi/api/funcs.py
+++ hngApi/api/funcs.py
@@ -1,18 +1,20 @@
 import re
 
-ADDITION = ['addition','add','sum','plus','+']
-SUBTRACTION = ['subtraction','subtract','minus','-']
-MULTIPLICATION = ['times','multiply','*', 'multiplication']
+ADDITION = ['addition','add','sum','plus','+', 'added']
+SUBTRACTION = ['subtraction','subtract','minus','-', 'subtracted']
+MULTIPLICATION = ['times','multiply','*', 'multiplication', 'multiplied']
 
 def translate_text(text):
     operations = ADDITION+SUBTRACTION+MULTIPLICATION
+    text = re.sub(r'[^\w]', ' ', text)
     numbers = re.findall('[0-9]+', text)
+    # remove all symbols then split
     textList = text.split()
     command = ''
     for a in textList:
-        if a in operations:
+        print(a)
+        if a.lower() in operations:
             command = a
-        break    
     number1 = numbers[0]
     number2 = numbers[1]
     number1 = int(number1)
