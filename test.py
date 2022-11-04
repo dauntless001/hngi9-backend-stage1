93eb788f328cf11dae0b8b351265d6b369e01c40
dauntless <suntopmultimedia@gmail.com>
2022-11-04 07:50:23 +0100

    test

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
 
 url = 'https://hngi9.herokuapp.com/arithmetic/'
-res = requests.post(url, data={'operation_type':'the addition of certain two numbers of which the first one is 14 and the second one is 20 is?'})
+res = requests.post(url, data={})
 print(res.json())
